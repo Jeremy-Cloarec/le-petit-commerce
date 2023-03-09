c9d6c217c7e13dce1ce07e92ba22268456df5ce8
Jeremy-Cloarec <jeremycloarec@msn.com>
2023-03-09 21:01:41 +0100

    Page redv

---
+++ petitCommerce/js/slider1.js
@@ -0,0 +1,47 @@
+
+
+
+
+var swiper = new Swiper(".slide-content", {
+   
+    slidesPerView: 1,
+    // slidesPerGroup: 1,
+    centeredSlides:false,
+    // slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    spaceBetween:24,
+    loop:true,
+    loopFillGroupWhithBlank:true,
+
+   
+    breakpoints: {
+      576: {
+        slidesPerView: 3,
+        
+      },
+      992: {
+        slidesPerView: 5,
+      },
+    },
+
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets:true,
+    },
+  });
+
+
+
+
+
