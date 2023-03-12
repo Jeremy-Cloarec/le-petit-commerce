778122cdf14234b9d30c48584c4374d3ae29aacc
Jeremy-Cloarec <jeremycloarec@msn.com>
2023-03-12 20:47:37 +0100

    Page produit bientot la fin

--- petitCommerce/js/pageProduit.js
+++ petitCommerce/js/pageProduit.js
@@ -1,6 +1,5 @@
 
-
-
+//Slider 
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
@@ -25,6 +24,45 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+//Accordeon
+
+const accordeonPP = document.querySelectorAll('.accordeonPP')
+
+
+accordeonPP.forEach((item, index)=>{
+
+  let accordeonPPTitleIcone = item.querySelector('.accordeonPPTitleIcone');
+
+  accordeonPPTitleIcone.addEventListener("click", ()=>{
+    item.classList.toggle("open");
+    let descriptionAccordeonPP = item.querySelector(".descriptionAccordeonPP");
+
+    if(item.classList.contains("open")){
+      descriptionAccordeonPP.style.height=`${descriptionAccordeonPP.scrollHeight}px`;
+      item.querySelector('.accordeonPPPlus').innerHTML='-';
+    }else{
+      descriptionAccordeonPP.style.height="0px";
+      item.querySelector('.accordeonPPPlus').innerHTML='+';
+    }
+    removeOpen(index) //calling the function and also passing the index number of the clicked header  
+  })
+
+})
+
+function removeOpen(index1){
+  accordeonPP.forEach((item2, index2)=> {
+    if(index1 != index2){
+      item2.classList.remove("open");
+      let des = item2.querySelector(".descriptionAccordeonPP");
+      des.style.height="0px";
+      item2.querySelector('.accordeonPPPlus').innerHTML='+';
+    }
+  })
+
+}
+
+
 
 
 
